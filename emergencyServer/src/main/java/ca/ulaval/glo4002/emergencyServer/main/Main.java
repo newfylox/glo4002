ca83f5f762f15c7007eccd96ab75826dad009b96
maxime_cl <maxi297@hotmail.com>
2013-01-31 13:48:33 -0500

    adding emergencyServer and centralServer with POST request

---
+++ emergencyServer/src/main/java/ca/ulaval/glo4002/emergencyServer/main/Main.java
@@ -0,0 +1,28 @@
+package ca.ulaval.glo4002.emergencyServer.main;
+
+import org.eclipse.jetty.server.Server;
+import org.eclipse.jetty.servlet.ServletContextHandler;
+import org.eclipse.jetty.servlet.ServletHolder;
+
+import com.sun.jersey.spi.container.servlet.ServletContainer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		int PORT = 8081;
+
+		Server server = new Server(PORT);
+		ServletContextHandler servletContextHandler = new ServletContextHandler(
+				server, "/");
+		ServletHolder jerseyServletHolder = new ServletHolder(ServletContainer.class);
+		jerseyServletHolder.setInitParameter(
+				"com.sun.jersey.config.property.resourceConfigClass",
+				"com.sun.jersey.api.core.PackagesResourceConfig");
+		jerseyServletHolder.setInitParameter(
+				"com.sun.jersey.config.property.packages",
+				"ca.ulaval.glo4002.emergencyServer.rest");
+		servletContextHandler.addServlet(jerseyServletHolder, "/*");
+		server.start();
+
+	}
+}
