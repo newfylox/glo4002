7206a40f2a3829c5c1e2beec6cb6528fe887af1c
Alexandre Girard <girarda.92@gmail.com>
2013-02-08 10:42:02 -0500

    Small bugfix in cancelDelay and checkstyle.

--- client/src/main/java/ca/ulaval/glo4002/client/DelayManager.java
+++ client/src/main/java/ca/ulaval/glo4002/client/DelayManager.java
@@ -5,40 +5,42 @@ import java.util.TimerTask;
 
 public class DelayManager {
 
-	private Timer timer;
-	private DelayResponder responder;
-	private Object identifier;
+  private Timer timer;
+  private DelayResponder responder;
+  private Object identifier;
 
-	public DelayManager(DelayResponder responder) {
-		this.responder = responder;
-	}
+  public DelayManager(DelayResponder responder) {
+    this.responder = responder;
+  }
 
-	public void startDelay(int delay, Object identifier) {
-		if (timer != null) {
-			timer.cancel();
-			timer = null;
-		}
-		timer = new Timer();
-		timer.schedule(new TimerAction(), delay * 1000);
-	}
+  public void startDelay(int delay, Object identifier) {
+    if (timer != null) {
+      timer.cancel();
+      timer = null;
+    }
+    timer = new Timer();
+    timer.schedule(new TimerAction(), delay * 1000);
+  }
 
-	private class TimerAction extends TimerTask {
-		@Override
-		public void run() {
-			responder.delayExpired(identifier);
-			timer.cancel();
-			timer = null;
-		}
-	}
+  private class TimerAction extends TimerTask {
+    @Override
+    public void run() {
+      responder.delayExpired(identifier);
+      timer.cancel();
+      timer = null;
+    }
+  }
 
-	public void cancelDelay() {
-		timer.cancel();
-		timer = null;
-		identifier = null;
-	}
+  public void cancelDelay() {
+    if (timer != null) {
+      timer.cancel();
+      timer = null;
+      identifier = null;
+    }
+  }
 
-	public boolean isRunning() {
+  public boolean isRunning() {
 
-		return timer != null;
-	}
+    return timer != null;
+  }
 }
--- client/src/main/java/ca/ulaval/glo4002/client/DetectionSignalAdapter.java
+++ client/src/main/java/ca/ulaval/glo4002/client/DetectionSignalAdapter.java
@@ -4,26 +4,25 @@ import ca.ulaval.glo4002.client.Signal.DetectorType;
 
 public class DetectionSignalAdapter {
 
-	protected SignalHandler detectorSignalHandler;
+  protected SignalHandler detectorSignalHandler;
 
-	public DetectionSignalAdapter() {
-		detectorSignalHandler = SignalHandler.getInstance();
-	}
+  public DetectionSignalAdapter() {
+    detectorSignalHandler = SignalHandler.getInstance();
+  }
 
-	protected DetectionSignalAdapter(SignalHandler signalHandler) {
-		detectorSignalHandler = signalHandler;
-	}
+  protected DetectionSignalAdapter(SignalHandler signalHandler) {
+    detectorSignalHandler = signalHandler;
+  }
 
-	public void sendSignalToSignalHandler(DetectorType detectorType,
-			int delayToContactEmergency) {
-		Signal signalSource = createSignal(detectorType,
-				delayToContactEmergency);
-		detectorSignalHandler.treatSignal(signalSource);
-	}
+  public void sendSignalToSignalHandler(DetectorType detectorType,
+      int delayToContactEmergency) {
+    Signal signalSource = createSignal(detectorType, delayToContactEmergency);
+    detectorSignalHandler.treatSignal(signalSource);
+  }
 
-	private Signal createSignal(DetectorType detectorType,
-			int delayToContactEmergency) {
-		return new Signal(detectorType, delayToContactEmergency);
-	}
+  public Signal createSignal(DetectorType detectorType,
+      int delayToContactEmergency) {
+    return new Signal(detectorType, delayToContactEmergency);
+  }
 
 }
