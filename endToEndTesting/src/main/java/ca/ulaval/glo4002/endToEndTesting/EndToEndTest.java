b5fe0aaf5ba7ff6780ea171f16ba009fd16eebce
Alexandre Girard <girarda.92@gmail.com>
2013-02-08 10:42:30 -0500

    End-to-end testing update.

--- endToEndTesting/src/main/java/ca/ulaval/glo4002/endToEndTesting/EndToEndTest.java
+++ endToEndTesting/src/main/java/ca/ulaval/glo4002/endToEndTesting/EndToEndTest.java
@@ -4,9 +4,13 @@ import static org.junit.Assert.*;
 
 import org.junit.After;
 import org.junit.Before;
+import org.junit.Ignore;
 import org.junit.Test;
 
 import ca.ulaval.glo4002.centralServer.main.CentralServer;
+import ca.ulaval.glo4002.client.DetectionSignalAdapter;
+import ca.ulaval.glo4002.client.Signal;
+import ca.ulaval.glo4002.client.SignalHandler;
 import ca.ulaval.glo4002.emergencyServer.main.EmergencyServer;
 
 public class EndToEndTest {
@@ -14,12 +18,17 @@ public class EndToEndTest {
   private String RESPONSE_TO_POST_REQUEST_FROM_CENTRAL = "POST request received at central server";
   private String RESPONSE_TO_POST_REQUEST = RESPONSE_TO_POST_REQUEST_FROM_CENTRAL
       + RESPONSE_TO_POST_REQUEST_FROM_EMERGENCY;
-  private String A_POST_REQUEST = "simple request";
+  private int TIME_TO_DISARM_THE_ALARM_WHEN_SECONDARY_DOOR_OPENS = 0;
+  private SignalHandler signalHandler;
+  private DetectionSignalAdapter detectSignal;
 
   @Before
   public void setUp() throws Exception {
     CentralServer.startServer();
     EmergencyServer.startServer();
+
+    signalHandler = SignalHandler.getInstance();
+    detectSignal = new DetectionSignalAdapter();
   }
 
   @After
@@ -28,11 +37,13 @@ public class EndToEndTest {
     EmergencyServer.stopServer();
   }
 
+  @Ignore
   @Test
   public void endToEndTest() {
-    Home home = new Home();
-    home.armSystem();
-    home.openMainDoor();
-    assertEquals(postRequestAnswer, RESPONSE_TO_POST_REQUEST);
+    detectSignal.sendSignalToSignalHandler(Signal.DetectorType.SecondaryDoor,
+        TIME_TO_DISARM_THE_ALARM_WHEN_SECONDARY_DOOR_OPENS);
+    String lastResponseFromServer = signalHandler.getLastResponseReceived();
+
+    assertEquals(RESPONSE_TO_POST_REQUEST, lastResponseFromServer);
   }
 }
