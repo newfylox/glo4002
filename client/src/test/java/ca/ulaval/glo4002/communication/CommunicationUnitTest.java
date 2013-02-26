6a1dcbab70eb08359d0ab9a88bb080808987ede6
Alex Gervais <alex.gervais@mac.com>
2013-02-26 14:32:30 -0500

    enlever un vieux fichier

--- client/src/test/java/ca/ulaval/glo4002/communication/CommunicationUnitTest.java
+++ client/src/test/java/ca/ulaval/glo4002/communication/CommunicationUnitTest.java
@@ -1,6 +1,14 @@
 package ca.ulaval.glo4002.communication;
 
+import static org.junit.Assert.*;
+
+import org.junit.Test;
 
 public class CommunicationUnitTest {
+
+    @Test
+    public void test() {
+        fail();
+    }
 
 }
