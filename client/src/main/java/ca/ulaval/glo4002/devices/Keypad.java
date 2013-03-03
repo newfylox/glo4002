6e1f1326333b349f2f887f6c44eba9c723fad033
Alexandre Girard <girarda.92@gmail.com>
2013-03-03 16:57:06 -0500

    Fix NIP.

--- client/src/main/java/ca/ulaval/glo4002/devices/Keypad.java
+++ client/src/main/java/ca/ulaval/glo4002/devices/Keypad.java
@@ -13,36 +13,40 @@ public class Keypad {
         this.validPIN = DEFAULT_PIN;
     }
 
-    public void armSystem(final String pin) throws BadStateException,
+    public void armSystem(final String PIN) throws BadStateException,
             InvalidPINException {
-        validatePIN(pin);
-        alarmSystem.arm();
-    }
-
-    public void disarmSystem(final String pin) throws InvalidPINException {
-        validatePIN(pin);
-        alarmSystem.disarm();
-    }
-
-    public void changePIN(final String pin, final String newPIN)
-            throws InvalidPINException {
-        validatePIN(pin);
-        checkPINFormat(newPIN);
-        validPIN = newPIN;
-    }
-
-    protected boolean isPINValid(final String newPin) {
-        return validPIN == newPin;
-    }
-
-    private void validatePIN(final String pin) throws InvalidPINException {
-        if (pin != validPIN && pin != RAPID_PIN) {
+        if (isPINValid(PIN)) {
+            alarmSystem.arm();
+        } else {
             throw new InvalidPINException();
         }
     }
 
-    private void checkPINFormat(final String pin) {
-        if (!pin.matches("^[0-9]{5}$")) {
+    public void disarmSystem(final String PIN) throws InvalidPINException {
+        if (isPINValid(PIN)) {
+            alarmSystem.disarm();
+        } else {
+            throw new InvalidPINException();
+        }
+    }
+
+    public void changePIN(final String PIN, final String newPIN)
+            throws InvalidPINException {
+        if (isPINValid(PIN)) {
+            checkPINFormat(newPIN);
+            validPIN = newPIN;
+        } else {
+            throw new InvalidPINException();
+        }
+    }
+
+    // Protected for testing purposes
+    protected boolean isPINValid(final String newPIN) {
+        return validPIN == newPIN || RAPID_PIN == newPIN;
+    }
+
+    private void checkPINFormat(final String PIN) {
+        if (!PIN.matches("^[0-9]{5}$")) {
             throw new PINFormatForbiddenException();
         }
     }
