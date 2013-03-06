6853f30c9e0a351c49b2a36884bc67b0911271d6
KevinDion <dion.kevin.1@gmail.com>
2013-03-06 04:29:50 -0500

    Configuration des pom de maven avec un master pom

--- common/src/main/java/ca/ulaval/glo4002/common/requestSender/GETRequestSender.java
+++ common/src/main/java/ca/ulaval/glo4002/common/requestSender/GETRequestSender.java
@@ -5,7 +5,7 @@ import com.sun.jersey.api.client.ClientResponse;
 import com.sun.jersey.api.client.WebResource;
 
 public class GETRequestSender extends HTTPRequestSender {
-
+    
     public GETRequestSender(int port) {
         super(port);
     }
